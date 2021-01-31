e3c9f63bf5ea2d57b16e4cc3d06879a1dde03529
David Wengier <git@wengier.com>
2021-02-01 08:08:49 +1100

    Add project files.

---
+++ Net48Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MySG;
+
+namespace ConsoleApp275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Say.Hello());
+        }
+    }
+}
---
+++ SourceGenerator/Generator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGenerator
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var source = @"namespace MySG
+{
+    public static class Say
+    {
+        public static string Hello() => ""Hello"";
+    }
+}";
+
+            if (source != null)
+            {
+                context.AddSource("generated.cs", source);
+            }
+        }
+    }
+}
